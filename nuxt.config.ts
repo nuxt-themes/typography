9c6fe505aa2b0f94661009c3ef2b209e2afef670
Sébastien Chopin <seb@nuxtjs.com>
2022-11-09 12:50:51 +0100

    chore: move components to components/global

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,9 @@
-import { logger, createResolver } from '@nuxt/kit'
+import { logger } from '@nuxt/kit'
 import { version } from './package.json'
 
-const { resolve } = createResolver(import.meta.url)
 logger.success(`Using Nuxt Typography v${version}`)
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  components: [
-    { path: resolve('./components'), global: true, prefix: '' }
-  ],
   modules: ['nuxt-icon', '@nuxt-themes/tokens']
 })
