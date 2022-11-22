693c2d35f4346a9185484938fa76b755211a4791
Yaël Guilloux <yael.guilloux@gmail.com>
2022-11-22 01:16:18 +0100

    fix(imports): add missing imports

--- components/ProseCodeCopyButton.vue
+++ components/ProseCodeCopyButton.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useClipboard } from '@vueuse/core'
 
 const props = defineProps({
--- components/global/ProseCode.vue
+++ components/global/ProseCode.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { PropType } from 'vue'
+import { ref } from 'vue'
 import type { Lang } from 'shiki-es'
 
 defineProps({
